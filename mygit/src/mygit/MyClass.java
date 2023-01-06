a3488e097baecfb1691f362acaefc0e3eff34030
ShivendraSingh <shivendra.singh@thrymr.net>
2023-01-06 10:04:26 +0530

    shivendra changes

--- mygit/src/mygit/MyClass.java
+++
@@ -1,9 +0,0 @@
-package mygit;
-
-public class MyClass {
-	
-	public static void main(String[] args) {
-		System.out.println("Hi GitHub World");
-	}
-
-}
---
+++ src/main/java/com/practice/shiva/FirstGitPractice.java
@@ -0,0 +1,28 @@
+package com.practice.shiva;
+
+public class FirstGitPractice {
+	
+	private long id;
+	private String name;
+	private String email;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+
+}
