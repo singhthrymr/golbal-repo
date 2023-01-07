541a3325c6e02cc09e090df332e77bec432a218f
thrymr <sunil@thrymr.net>
2023-01-07 18:23:15 +0530

    basic security added

--- src/test/java/com/security/SecurityApplicationTests.java
+++ src/test/java/com/security/SecurityApplicationTests.java
@@ -1,10 +1,10 @@
-package com.practice;
+package com.security;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GitPracticeBackendApplicationTests {
+class SecurityApplicationTests {
 
 	@Test
 	void contextLoads() {
