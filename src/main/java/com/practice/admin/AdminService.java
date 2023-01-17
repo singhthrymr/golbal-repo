b90aa8e2e8222a9590a676213acde41e2e8a48a8
thrymr <sunil@thrymr.net>
2023-01-17 09:52:13 +0530

    admin service added

---
+++ src/main/java/com/practice/admin/AdminService.java
@@ -0,0 +1,14 @@
+/**
+ * Created By Sunil Verma
+ * Date: 17/01/23
+ * Time: 9:48 AM
+ * Project Name: git-practice-backend
+ */
+
+package com.practice.admin;
+
+public interface AdminService {
+
+    public void addUser();
+
+}
---
+++ src/main/java/com/practice/admin/impl/AdminServiceImpl.java
@@ -0,0 +1,18 @@
+/**
+ * Created By Sunil Verma
+ * Date: 17/01/23
+ * Time: 9:49 AM
+ * Project Name: git-practice-backend
+ */
+
+package com.practice.admin.impl;
+
+import com.practice.admin.AdminService;
+
+public class AdminServiceImpl implements AdminService {
+
+    @Override
+    public void addUser() {
+        System.out.println("Hi user added");
+    }
+}
