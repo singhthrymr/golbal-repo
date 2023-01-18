6fced4422fc1b0586885f668aabd14f83cc1844e
Tusar009 <sahootusar642@gmail.com>
2023-01-18 11:57:48 +0530

    tusarsahoo changed

--- src/main/java/com/practice/admin/impl/AdminServiceImpl.java
+++ src/main/java/com/practice/admin/impl/AdminServiceImpl.java
@@ -16,6 +16,6 @@ public class AdminServiceImpl implements AdminService {
         System.out.println("Hi user added");
         System.out.println("dilip.gadhor@thrymr.com");
         System.out.println("Shivendra.singh@thyrmr.com");
-        System.out.println("Tusar want to change somting");
+        System.out.println("Tusar want to  change somting");
     }
 }
